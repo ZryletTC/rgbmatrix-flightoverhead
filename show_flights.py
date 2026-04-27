13367db78668db5b537fa0140f70183a79f34b12
Tyler Pennebaker <tntptntp@gmail.com>
2026-04-26 23:11:52 -0700

    Refactor functions organization

--- show_flights.py
+++ show_flights.py
@@ -5,6 +5,7 @@ import json
 from PIL import BdfFontFile, Image, ImageDraw
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
+
 options = RGBMatrixOptions()
 options.hardware_mapping = "adafruit-hat"
 options.gpio_slowdown = 2
@@ -12,40 +13,45 @@ options.cols = 64
 
 matrix = RGBMatrix(options=options)
 
+
 with open("/home/pi/adafruit-rgb-led-matrix/fonts/5x8.bdf", "rb") as ff:
     fontfile = BdfFontFile.BdfFontFile(ff)
     font = fontfile.to_imagefont()
 
 
-def show_flights(testarray=None):
-    if testarray:
-        data = {"aircraft":[{"flight":test_callsign} for test_callsign in testarray]}
-    else:
-        with open("/run/dump1090-fa/aircraft.json", "r") as f:
-            data = json.load(f)
+def get_flights():
+    with open("/run/dump1090-fa/aircraft.json", "r") as f:
+        data = json.load(f)
 
-    img = Image.new('RGB', (64, 32))
-    draw = ImageDraw.Draw(img)
-
-    ypos = 0
+    lines = []
     for aircraft in data['aircraft']:
         if 'flight' in aircraft.keys():
-            callsign = aircraft['flight']
-            draw.text((0,ypos), callsign, fill=(255,255,255), font=font)
-            ypos += 9
+            lines.append(aircraft['flight'])
+
+    return lines
+
+
+def display_text(text_array=[]):
+    img = Image.new('RGB', (64, 32))
+    draw = ImageDraw.Draw(img)
+    ypos = 0
+
+    for line in text_array:
+        draw.text((0,ypos), line, fill=(255,255,255), font=font)
+        ypos += 9
 
     matrix.SetImage(img)
 
 
 def watch_flights():
     while True:
-        show_flights()
+        if TEST:
+            lines = ["testing...", "1", "2", "3"]
+        else:
+            lines = get_flights()
+        display_text(text_array=lines)
         time.sleep(2)
 
 
 if __name__ == "__main__":
-    if TEST:
-        show_flights(['SKW5389','testing...'])
-        time.sleep(10)
-    else:
-        watch_flights()
+    watch_flights()

4fc578c9da0a07b4ddfb427e51beb93d7a864f94
Tyler Pennebaker <pennebaker@ucsb.edu>
2026-05-26 21:03:32 -0700

    STY: Format with black

--- show_flights.py
+++ show_flights.py
@@ -28,17 +28,16 @@ SETTINGS = {
     "fg_color_error": "#f00",
     "lat": 34.427746,
     "lon": -119.840742,
-    "radius": 4
+    "radius": 4,
 }
 
 try:
-    with open(HERE_DIR/"settings.json", "r") as f:
+    with open(HERE_DIR / "settings.json", "r") as f:
         json_settings = json.load(f)
         for key, val in json_settings.items():
             SETTINGS[key] = val
 except OSError:
     print("settings.json not found. Using default settings.")
-
 
 
 with open("/home/pi/adafruit-rgb-led-matrix/fonts/5x8.bdf", "rb") as ff:
@@ -48,60 +47,64 @@ with open("/home/pi/adafruit-rgb-led-matrix/fonts/5x8.bdf", "rb") as ff:
 
 def get_distance(lat2, lon2):
     R = 6373  # Earth radius in km
-    lat1 = SETTINGS['lat']  # Receiver latitude
-    lon1 = SETTINGS['lon']  # Receiver longitude
+    lat1 = SETTINGS["lat"]  # Receiver latitude
+    lon1 = SETTINGS["lon"]  # Receiver longitude
 
     d_lat = np.deg2rad(lat2 - lat1)
     d_lon = np.deg2rad(lon2 - lon1)
     lat1_rads = np.deg2rad(lat1)
     lat2_rads = np.deg2rad(lat2)
 
-    a = (np.sin(d_lat/2)**2
-         + np.cos(lat1_rads)*np.cos(lat2_rads) * np.sin(d_lon/2)**2)
+    a = (
+        np.sin(d_lat / 2) ** 2
+        + np.cos(lat1_rads) * np.cos(lat2_rads) * np.sin(d_lon / 2) ** 2
+    )
 
     return 2 * R * np.arcsin(np.sqrt(a))
 
 
 def get_aircraft_distance(aircraft):
-    lat2 = aircraft['lat']  # Aircraft latitude
-    lon2 = aircraft['lon']  # Aircraft longitude
+    lat2 = aircraft["lat"]  # Aircraft latitude
+    lon2 = aircraft["lon"]  # Aircraft longitude
 
     return get_distance(lat2, lon2)
 
 
 def in_area(lat, lon):
-    if SETTINGS['selection_method'] == 'rect':
-        return (lat > SETTINGS['lat_min'] and
-                lat < SETTINGS['lat_max'] and
-                lon > SETTINGS['lon_min'] and
-                lon < SETTINGS['lon_max'])
-    elif SETTINGS['selection_method'] == 'radius':
+    if SETTINGS["selection_method"] == "rect":
+        return (
+            lat > SETTINGS["lat_min"]
+            and lat < SETTINGS["lat_max"]
+            and lon > SETTINGS["lon_min"]
+            and lon < SETTINGS["lon_max"]
+        )
+    elif SETTINGS["selection_method"] == "radius":
         dist = get_distance(lat, lon)
         print(f"Distance: {dist}km")
-        return get_distance(lat, lon) < SETTINGS['radius']
+        return get_distance(lat, lon) < SETTINGS["radius"]
 
 
 def is_overhead(aircraft):
     keys = aircraft.keys()
 
-    if 'flight' not in keys:
+    if "flight" not in keys:
         return False
 
     try:
-        alt = aircraft['alt_baro']
-        lat = aircraft['lat']
-        lon = aircraft['lon']
+        alt = aircraft["alt_baro"]
+        lat = aircraft["lat"]
+        lon = aircraft["lon"]
     except KeyError as e:
         print(f"Key not found in json. {e}", file=sys.stderr)
         print(f"JSON Data: {aircraft}")
         return False
 
-    return (alt < 5000 and in_area(lat, lon))
+    return alt < 5000 and in_area(lat, lon)
 
 
 def get_overhead_aircraft():
     if TEST:
-        json_path = HERE_DIR/"test.json"
+        json_path = HERE_DIR / "test.json"
     else:
         json_path = "/run/dump1090-fa/aircraft.json"
 
@@ -109,7 +112,7 @@ def get_overhead_aircraft():
         data = json.load(f)
 
     aircraft_list = []
-    for aircraft in data['aircraft']:
+    for aircraft in data["aircraft"]:
         if is_overhead(aircraft):
             aircraft_list.append(aircraft)
 
@@ -126,15 +129,15 @@ def get_overhead_aircraft():
 def get_aircraft_info(aircraft):
     if aircraft is None:
         return None
-    return [aircraft['flight']]
+    return [aircraft["flight"]]
 
 
 def display_text(text_array=None, error=False):
     if text_array is None:
         matrix.Clear()
     else:
-        fg_color = SETTINGS['fg_color'] if not error else SETTINGS['fg_color_error']
-        img = Image.new('RGB', (64, 32), SETTINGS['bg_color'])
+        fg_color = SETTINGS["fg_color"] if not error else SETTINGS["fg_color_error"]
+        img = Image.new("RGB", (64, 32), SETTINGS["bg_color"])
         draw = ImageDraw.Draw(img)
         ypos = 0
 
